38bf1b8e071a2d12ef44def150ef4c84a2fd7e53
Elandland <bodan99@gmail.com>
2022-09-09 21:40:12 +0900

    AppConfig Refactoring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,31 +2,38 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDicountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 
 //환경 구성 자체를 이제는 Impl이 할게 아니라 AppConfig가 전체적으로 총괄
+//이런 애를 DI container라고 함
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
-
-    private MemberRepository memberRepository() {       //가시성이 높게 refactoring
+    @Bean
+    public MemberRepository memberRepository() {       //가시성이 높게 refactoring
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy(){             //역할과 구현 클래스가 한눈에 보이도록
-        return new FixDicountPolicy();
+        //return new FixDicountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,14 +4,19 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        //emberService memberService = new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        memberService memberService = new MemberServiceImpl();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);     //스프링 컨테니어라고 생각. 모든걸(객체, @bean등등) 다 관리
+        MemberService memberService = applicationContext.getBean("memberService",MemberService.class);
 
         Member member = new Member(1L, "chansol", Grade.VIP);
 
